1d6396362deb80bac511f65e1a239dac374f5130
Airbrett <mixmasterbrett@gmail.com>
2023-02-11 23:03:07 -0700

    support static linking

--- packages/Irrlicht-svn/IrrlichtConfig.cmake
+++ packages/Irrlicht-svn/IrrlichtConfig.cmake
@@ -13,9 +13,14 @@ if (WIN32)
 	list(APPEND Irrlicht_LIBS ${CMAKE_INSTALL_PREFIX}/lib/Irrlicht.lib)
 endif()
 
+if("@BUILD_SHARED_LIBS@" STREQUAL "")
+	set(COMPILE_DEFS INTERFACE_COMPILE_DEFINITIONS _IRR_STATIC_LIB_)
+endif()
+
 set_target_properties(Irrlicht PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${Irrlicht_INCLUDE_DIR}
 	INTERFACE_LINK_LIBRARIES ${Irrlicht_LIBS}
+	${COMPILE_DEFS}
 )
 
 find_package_handle_standard_args(Irrlicht DEFAULT_MSG Irrlicht_INCLUDE_DIR Irrlicht_LIBS)
